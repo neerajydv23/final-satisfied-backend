c157af6f6fdd710d5869b4727e0f04d98e77d42d
arpitsahuu <arpitsahu358@gmail.com>
2024-03-12 00:16:50 +0530

    ho

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-	origin: 'http://localhost:3000',
+	origin: 'https://final-satisfiend-job.vercel.app',
 	credentials: true,
 	optionsSuccessStatus: 200 // Address potential preflight request issues
 }));
