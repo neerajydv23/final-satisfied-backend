08473fca24f08478fa3b895807d1ca7f8b6f70ba
arpitsahuu <arpitsahu358@gmail.com>
2024-03-24 23:24:42 +0530

    update origin

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const allowedOrigins = [
 
 app.use(cors({
 	// origin: 'https://demoto.vercel.app',
-	origin: 'http://localhost:3000',
+	origin: 'https://satisfiedjob.com',
 	credentials: true,
 	optionsSuccessStatus: 200 // Address potential preflight request issues
 }));
